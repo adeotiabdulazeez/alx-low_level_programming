896a256408acf9c51556a1aefa8511242e68986b
Azeez Adeoti <adeotiabdulaziz@gmail.com>
2022-12-10 20:08:04 -0800

    dfg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**                                                                                                                                     
  * main - Entry                                                                                                                         
  * Return: Always 0                                                                                                                     
- */
+ */ 
 int main(void)
 {
-        puts("\"Programming is like building a multi lingual puzzle);
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
