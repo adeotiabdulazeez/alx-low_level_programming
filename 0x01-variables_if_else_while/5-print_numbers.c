b765a79d5aa20b3121e6eab5ffa08e55c978b5a3
Azeez Adeoti <adeotiabdulaziz@gmail.com>
2022-12-21 08:27:41 -0800

    df

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -dff
+ * thtas shoos
+ * Return : always 0 (sucess)
+ */
+int main(void)
+{
+
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("/n");
+	return (0);
+}
+
