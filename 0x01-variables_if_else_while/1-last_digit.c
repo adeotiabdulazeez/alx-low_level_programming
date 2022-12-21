5515ef7f2674623160a9379efb4f009317d12ec4
Azeez Adeoti <adeotiabdulaziz@gmail.com>
2022-12-21 06:16:01 -0800

    ghf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - print the last digit of the random
- * number stored in the variable
- * Return: always 0 (success)
+ * main - prints
+ * number syire in vatia
+ * Return : always 0 success
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
         printf("the last digit of %d is %d", n, n % 10);
-        if (n % 10 > 5);
+        if (n % 10 > 5){
         printf("and is greater than 5\n");
-        else if (n % 10 == 0);{
+	} else if (n % 10 == 0){
         printf("and is 0/n");
-        else  (n % 10 < 6 && n % 10 != 0);
+	} else  (n % 10 < 6 && n % 10 != 0){
         printf("and is less than 6 and not 0\n");
+	}
         return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,20 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - print the last digit of the random
- * number stored in the variable
- * Return: always 0 (success)
+ * main - print results
+ * followed by a new line
+ * Return :always (0) ( success)
  */
-int main(void)
- {
-         int n;
 
-         srand(time(0));
-         n = rand() - RAND_MAX / 2;
-         printf("the last digit of %d is %d", n, n % 10);
-	 if (n % 10 > 5);
-         printf("and is greater than 5\n");
-	 else if (n % 10 == 0);
-	 printf("and is 0/n");
-	 else if (n % 10 < 6 && n % 10 != 0);
-	 printf("and is less than 6 and not 0\n");
-	 return 0;
+int main(void)
+{
+	char ch;
 
- }
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
