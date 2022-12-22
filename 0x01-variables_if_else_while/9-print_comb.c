1054b1c6d55780a1838b8b76394dd29c9a71a41c
Azeez Adeoti <adeotiabdulaziz@gmail.com>
2022-12-21 20:07:25 -0800

    fgh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main-goód
+ * the dhd
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
